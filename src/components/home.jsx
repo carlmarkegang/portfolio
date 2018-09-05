08ba07d793aff70ecd70590a73ac64a96d74de4a
carlmarkegang <carl.markegang@linasmatkasse.se>
2018-09-05 13:37:11 +0200

    security and some dummy text

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -5,8 +5,14 @@ class Home extends Component {
     render() {
 
         return (
-            <div>
-                Home
+            <div className="homeContainer">
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit. </h3>
+                <p>Integer efficitur, ante a ullamcorper cursus, lacus tellus tempor turpis, rhoncus fringilla turpis diam a est.
+                    Sed tristique sem tortor, ut posuere lorem vehicula vel. Maecenas laoreet bibendum varius. Morbi maximus rhoncus purus vel congue.
+                    Etiam eu leo condimentum, vestibulum ante a, varius turpis. Sed lectus orci, pretium vitae lectus at, volutpat porta lectus.
+                    Nulla facilisi. Suspendisse a arcu erat. Donec nec finibus sapien, ac sagittis enim. Sed rhoncus justo vitae ligula mattis accumsan.
+                    Donec arcu lectus, interdum ut congue mattis, tempor nec quam. Proin lacus nisi, facilisis finibus est ac, viverra accumsan eros.</p>
+                    <a href="/project" className="projectButtonLink"><div className="projectButton">Projects</div></a>
             </div>
         );
 
