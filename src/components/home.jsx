ed9dc8906b81b54dd0d5d9ad98d64cb5cb30b49d
carlmarkegang <huledule@hotmail.com>
2018-09-17 20:24:59 +0200

    mobile fix

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -7,7 +7,7 @@ class Home extends Component {
         return (
             <div className="homeContainer container">
                 <h3>Carl Markegång</h3>
-                <p>Lösningsorienterad och ansvarstagande person på 24 år.</p>
+                <p>Lösningsorienterad person på 24 år.</p>
 
                 <p>Just nu bosatt i Kalhäll. Några av mina intressen är natur, PC-spel..</p>
                 <span className="projectButtonLink"><div className="projectButton" link='/project'
