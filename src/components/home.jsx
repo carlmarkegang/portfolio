bc5fce7a9b0c05a554829294f7c7c3731c098887
carlmarkegang <huledule@hotmail.com>
2018-09-26 18:37:18 +0200

    image and update description

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -6,11 +6,16 @@ class Home extends Component {
 
         return (
             <div className="homeContainer container">
-                <h3>Carl Markegång</h3>
-                <p>Lösningsorienterad person på 24 år.</p>
+                <h3 className="homeHeader">Carl Markegång</h3>
+                <img src="/img/carl_markegang.png" className="homeMeImage" alt="Carl Markegång"/>
+                <p className="homeIntroduction">Hej!<br />
+                    Jag är 24 årig kille just nu bosatt i Kallhäll.<br />
+                    Utrustad med ett livslångt intresse för teknik så är jag garanterat personen ni söker!
 
-                <p>Just nu bosatt i Kalhäll. Några av mina intressen är natur, PC-spel och programering.</p>
-                <p><a href="http://markegang.se/mitt-cv.pdf">Se mitt CV</a></p>
+                </p>
+
+                <p>Vill du veta mer? <br />
+                <a href="http://markegang.se/mitt-cv.pdf">Se mitt CV</a></p>
                 <span className="projectButtonLink"><div className="projectButton" link='/project'
                                                          onMouseMove={this.props.onMouseMove}
                                                          onMouseDown={this.props.onMouseDown}>Projekt och Erfarenheter</div>
