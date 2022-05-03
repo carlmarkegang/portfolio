ef4919ce6491e38530a2f436a2abe26ae33d895d
carlmarkegang <huledule@hotmail.com>
2022-05-03 17:38:47 +0200

    Uppdaterar innehåll och inkluderar CV

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -8,7 +8,8 @@ class Home extends Component {
             <div className="homeContainer container">
                 <h3 className="homeHeader">Carl Markegång</h3>
                 <img src="/img/carl_markegang.png" className="homeMeImage" alt="Carl Markegång"/>
-                <p className="homeIntroduction">Hej!</p>
+                <p className="homeIntroduction">Hej!<p>Se mitt <a href="cv.pdf" target="_blank">CV</a></p></p>
+				
                 <span className="projectButtonLink"><div className="projectButton" link='/project'
                                                          onMouseMove={this.props.onMouseMove}
                                                          onMouseDown={this.props.onMouseDown}>Projekt och Erfarenheter</div>
